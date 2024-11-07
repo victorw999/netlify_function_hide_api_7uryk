3a91f14fe93e32e138a53c316ff01df7b8a87702
Victor Wong <=>
2024-11-07 14:46:46 -0800

    test

--- functions/token-hider/token-hider.js
+++ functions/token-hider/token-hider.js
@@ -1,8 +1,9 @@
 exports.handler = async function (event, context) {
   const value = process.env.HOOAH;
+  const { TOGGL_API, HOOAH } = process.env
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ msg: `Value of MY_IMPORTANT_VARIABLE is ${value}.` }),
+    body: JSON.stringify({ msg: `token-hider.js: \n Value is ${value}. \n TOGGL_API: ${TOGGL_API}, \n HOOAH:${HOOAH}` }),
   };
 };
--- public/script.js
+++ public/script.js
@@ -1,13 +1,20 @@
 window.onload = function () {
   const message = document.getElementById("message")
-  document.getElementById("button").addEventListener("click", () => {
-    console.log("hello", message)
+  document.getElementById("button").addEventListener("click", async () => {
+
+    // #1
+    const response1 = await fetch("/.netlify/functions/hello-world?name=Netlify");
+    const data1 = await response1.json();
+    console.log("(1)" + data1.msg);
+    message.innerHTML = "(2)" + data1.msg;
+
     // fetch("/.netlify/functions/hello-world?name=" + "Netlify")
     //   .then((data) => data.json())
     //   .then(({ msg }) => console.log("(1)" + msg) || (message.innerHTML = "(2)" + msg))
 
-    fetch("/.netlify/functions/api-hider")
+    // #2
+    await fetch("/.netlify/functions/token-hider")
       .then((data) => data.json())
-      .then(({ msg }) => console.log("(1)" + msg) || (message.innerHTML = "(2)" + msg))
+      .then(({ msg }) => console.log("new change 22222 \n (1)" + msg) || (message.innerHTML = "(2)" + msg))
   })
 }
